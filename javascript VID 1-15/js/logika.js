9e23e3a126b494031b4626f98ba4ae3b5002fe90
Panca Aji Nugraha <78069598+panji665@users.noreply.github.com>
2022-01-09 22:20:52 +0700

    Add files via upload

---
+++ javascript VID 1-15/js/logika.js
@@ -0,0 +1,27 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+//OR
+// nilai akan bernilai true jika salah satu true
+
+console.log( d || b || a);
+
+//AND
+//nilai akan bernilai false jika salah satu false  
+console.log(a && d && c);
+
+let x = 8;
+let y = 7;
+
+console.log(x > y);
+console.log(x < y); 
+console.log(x == y);
+console.log(x >= y);
+console.log(x <= y);
+
+let lima = "5";
+let limo = 5;
+
+console.log(lima === limo);
